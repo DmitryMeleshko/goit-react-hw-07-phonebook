a0eb38b1bb4f42ffae1408bc24ed26b42530f004
DmitryMeleshko <meleshko.triolcorp@gmail.com>
2023-05-28 22:24:38 +0300

    create hw-07-phonebook

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,51 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  selectIsLoading,
+  selectError,
+  selectVisibleContacts,
+} from 'redux/selectors';
+
+import { fetchContacts, deleteContact } from 'redux/operations';
+
+import { ContactItem } from 'components/ListItem/ListItem';
+import { List, Error } from './ContactList.styled';
+import { EmptyListIcon } from 'components/EmptyList/EmptyList';
+import { Loader } from 'components/Loader/Loader';
+
+export const ContactList = () => {
+  const contacts = useSelector(selectVisibleContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const dispatch = useDispatch();
+
+  const IsEmpty = contacts.length === 0;
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      {error && <Error>{error}</Error>}
+      {IsEmpty ? (
+        <EmptyListIcon />
+      ) : (
+        <List>
+          {contacts.map(({ id, name, number }) => (
+            <ContactItem
+              key={id}
+              name={name}
+              number={number}
+              onDelete={() => {
+                dispatch(deleteContact(id));
+              }}
+            />
+          ))}
+        </List>
+      )}
+    </>
+  );
+};
